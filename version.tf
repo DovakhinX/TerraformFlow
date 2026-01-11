63906d50909bbe49694beb339de884ba46a76de8
DovakhinX <sunwin60@gmail.com>
2026-01-11 22:11:55 +0400

    Github actions added

--- version.tf
+++ version.tf
@@ -7,5 +7,20 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "terrapro-789"
+    storage_account_name = "terrapro7890"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+  }
 }
+
+
+
+
+
+
+
+
 
